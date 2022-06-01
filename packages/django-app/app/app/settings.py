f8dd9da8361bf2b1d8038f60c562c4f7804452f6
Jesse Snyder <jessetsnyder@gmail.com>
2022-05-31 20:09:41 -0600

    add spectre. add slippers. fix media and static urls

--- packages/django-app/app/app/settings.py
+++ packages/django-app/app/app/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # contrib
+    'slippers',
 
     # own
     'core',
@@ -130,15 +131,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = os.environ.get('STATIC_URL', '/code/app/static/')
+STATIC_URL = os.environ.get('STATIC_URL', '/static/')
 
 # path to static directory in docker container.
 # this is where files are created when running `collectstatic`
-STATIC_ROOT = os.environ.get('STATIC_ROOT', '/code/app/static/')
+STATIC_ROOT = os.environ.get('STATIC_ROOT', os.path.join(BASE_DIR, 'static'))
 
 # https://docs.djangoproject.com/en/4.0/topics/files/
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = os.environ.get('MEDIA_URL', '/media/')
+MEDIA_ROOT = os.environ.get('MEDIA_ROOT', os.path.join(BASE_DIR, 'media'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
